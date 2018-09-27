c3841f97f76d69585c54a146451ae1ee2edb4dd4
Alessandro DiMarco <axdimarco@gmail.com>
2018-09-27 18:29:07 -0400

    Add abtesting service

--- entry.sh
+++ entry.sh
@@ -8,15 +8,18 @@ if [ -d "/usr/src/libraries/moonshine-core" ]; then
   pip install -e /usr/src/libraries/moonshine-core
 fi
 
-# create databases chrono, sms
-# python manage.py migrate chrono, sms
+# create databases abtesting-service, chrono, sms (moonshine-api)
+# python manage.py migrate abtesting-service, chrono, sms
 
-# generate super user for chrono and moonshine-api
+# generate super user for abtesting-service, chrono, moonshine-api
 # echo "from django.contrib.auth import get_user_model; User = geer_model(); User.objects.create_superuser('admin', 'admin@ordergroove.com', 'admin')" | python manage.py shell
 
-# import adventures moonshine-api
+# import adventures for moonshine-api
 # export PYTHONPATH="${PYTHONPATH}:/usr/src/app/moonshine-api"
 # python feed/import_all.py
+
+# Unsure how abtesting creds are used
+# og_testing_env=http://abtesting-service:8002
 
 # generate moonshine-api creds for chrono
 # client_id=
