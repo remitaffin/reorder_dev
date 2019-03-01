538acacf9aa451002314d80e5bd2a640488a6c6c
Alessandro DiMarco <axdimarco@gmail.com>
2019-03-01 17:47:30 -0500

    Update setup file

--- setup.sh
+++ setup.sh
@@ -1,3 +1,23 @@
+#! /bin/bash
+
+#
+# Setup Django apps
+#
+
 mysql -u root -pogpassw -e "create database abtesting-service";
 mysql -u root -pogpassw -e "create database chrono";
 mysql -u root -pogpassw -e "create database moonshine";
+
+docker-compose exec abtesting-service ./manage.py migrate
+docker-compose exec chrono ./manage.py migrate
+docker-compose exec moonshine ./manage.py migrate
+
+docker-compose exec abtesting-service bash -c 'echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser(\'admin\', \'admin@ordergroove.com\', \'admin\')" | python manage.py shell'
+docker-compose exec chrono bash -c 'echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser(\'admin\', \'admin@ordergroove.com\', \'admin\')" | python manage.py shell'
+docker-compose exec moonshine bash -c 'echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser(\'admin\', \'admin@ordergroove.com\', \'admin\')" | python manage.py shell'
+
+#
+# Moonshine-api
+#
+
+docker-compose exec moonshine-api bash -c 'export PYTHONPATH=/usr/src/app && python feed/import_all.py env=dev'
--- todo.md
+++ todo.md
@@ -2,26 +2,17 @@
 
 ## Reorder
 
-create databases abtesting-service, chrono, sms (moonshine-api)
-python manage.py migrate abtesting-service, chrono, sms
+generate abtesting creds
+OG_TESTING_AUTH_ID
+OG_TESTING_AUTH_SECRET
 
-generate super user for abtesting-service, chrono, moonshine-api
-echo "from django.contrib.auth import get_user_model; User = geer_model(); User.objects.create_superuser('admin', 'admin@ordergroove.com', 'admin')" | python manage.py shell
+generate moonshine-api creds
+CLIENT_ID=
+CLIENT_SECRET=
 
-import adventures for moonshine-api
-export PYTHONPATH="${PYTHONPATH}:/usr/src/app/moonshine-api"
-python feed/import_all.py
-
-Unsure how abtesting creds are used
-og_testing_env=http://abtesting-service:8002
-
-generate moonshine-api creds for chrono
-client_id=
-client_secret=
-
-generate chrono creds for moonshine-api
-chrono_auth_id=
-chrono_auth_secret=
+generate chrono creds
+CHRONO_AUTH_ID=
+CHRONO_AUTH_SECRET=
 
 
 ## Platform
