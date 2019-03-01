252c226445f45fbea33d5eb28c859239fe559026
Alessandro DiMarco <axdimarco@gmail.com>
2019-03-01 17:54:22 -0500

    Fix moonshine-api setup

--- setup.sh
+++ setup.sh
@@ -10,11 +10,11 @@ mysql -h 127.0.0.1 -u root -pogpassw -e "create database moonshine";
 
 docker-compose exec abtesting-service ./manage.py migrate
 docker-compose exec chrono ./manage.py migrate
-docker-compose exec moonshine ./manage.py migrate
+docker-compose exec moonshine-api ./manage.py migrate
 
 docker-compose exec abtesting-service bash -c 'echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser(\'admin\', \'admin@ordergroove.com\', \'admin\')" | python manage.py shell'
 docker-compose exec chrono bash -c 'echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser(\'admin\', \'admin@ordergroove.com\', \'admin\')" | python manage.py shell'
-docker-compose exec moonshine bash -c 'echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser(\'admin\', \'admin@ordergroove.com\', \'admin\')" | python manage.py shell'
+docker-compose exec moonshine-api bash -c 'echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser(\'admin\', \'admin@ordergroove.com\', \'admin\')" | python manage.py shell'
 
 #
 # Moonshine-api
