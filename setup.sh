5f1281d1392a276e2f9cb3651f5e138202b06cfe
Alessandro DiMarco <axdimarco@gmail.com>
2019-03-19 11:36:55 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,16 +8,17 @@
 docker-compose up -d mariadb
 sleep 10
 
+# Create dbs
 docker-compose exec mariadb mysql -u root -pogpassw -e "create database abtesting";
 docker-compose exec mariadb mysql -u root -pogpassw -e "create database chrono";
 docker-compose exec mariadb mysql -u root -pogpassw -e "create database moonshine";
 
-# Migrate db
+# Migrate dbs
 docker-compose run --rm abtesting-service ./manage.py migrate
 docker-compose run --rm chrono ./manage.py migrate
 docker-compose run --rm moonshine-api ./manage.py migrate
 
-# Create admin user
+# Create admin users
 docker-compose run --rm abtesting-service bash -c 'echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('"'admin'"', '"'admin@example.com'"', '"'pass'"')" | python manage.py shell'
 docker-compose run --rm chrono bash -c 'echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('"'admin'"', '"'admin@example.com'"', '"'pass'"')" | python manage.py shell'
 docker-compose run --rm moonshine-api bash -c 'echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('"'admin'"', '"'admin@example.com'"', '"'pass'"')" | python manage.py shell'
@@ -27,28 +28,9 @@ docker-compose run --rm abtesting-service bash -c 'echo "from oauth2_provider.mo
 docker-compose run --rm chrono bash -c 'echo "from oauth2_provider.models import Application; Application.objects.create(client_id='"'chrono-client-id'"', client_secret='"'chrono-secret'"', user_id=1, client_type='"'public'"', name='"'admin app'"', authorization_grant_type='"'client-credentials'"')" | python manage.py shell'
 docker-compose run --rm moonshine-api bash -c 'echo "from oauth2_provider.models import Application; Application.objects.create(client_id='"'moonshine-api-client-id'"', client_secret='"'moonshine-api-secret'"', user_id=1, client_type='"'public'"', name='"'admin app'"', authorization_grant_type='"'client-credentials'"')" | python manage.py shell'
 
-#
-# Moonshine-api
-#
-
+# Import adventures for moonshine-api
 docker-compose run --rm moonshine-api bash -c 'export PYTHONPATH=/usr/src/app && python feed/import_all.py env=dev'
 
-#
-# Whiskey/Lego
-#
-
-# mysql -h 127.0.0.1 -u root -pogpassw -e "create database ordergroove";
-# Use stg DB
-# https://ordergroove.atlassian.net/wiki/spaces/DEV/pages/48267266/Backend+environment+set-up
-
-#
-# Call-me-maybe
-#
-# mysql -h 127.0.0.1 -u root -pogpassw -e "create database callmemaybe"; NOT SETUP YET
-
-#
 # Stop services
-#
-
 docker-compose stop
 echo 'done!'
