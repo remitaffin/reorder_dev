392b0e1960474e6c9cc21e44bb90642bcd04856a
Alessandro DiMarco <axdimarco@gmail.com>
2019-05-15 17:21:40 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ function heading() {
 
 heading 'Starting...'
 
-docker-compose up -d mariadb rabbitmq
+docker-compose up -d
 ./wait-for-it.sh --strict rabbitmq:5672 -t 60 -- echo 'rabbitmq is up'
 ./wait-for-it.sh --strict mariadb:3306 -t 60 -- echo 'mariadb is up'
 
@@ -20,21 +20,21 @@ docker-compose exec mariadb mysql -u root -pogpassw -e "create database chrono";
 docker-compose exec mariadb mysql -u root -pogpassw -e "create database moonshine";
 
 heading 'Run migrations'
-docker-compose run --rm abtesting-service ./manage.py migrate
-docker-compose run --rm chrono ./manage.py migrate
-docker-compose run --rm moonshine-api ./manage.py migrate
+docker-compose run abtesting-service ./manage.py migrate
+docker-compose run chrono ./manage.py migrate
+docker-compose run moonshine-api ./manage.py migrate
 
 heading 'Create admin users'
-docker-compose run --rm abtesting-service bash -c 'echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('"'admin'"', '"'admin@example.com'"', '"'pass'"')" | python manage.py shell'
-docker-compose run --rm chrono bash -c 'echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('"'admin'"', '"'admin@example.com'"', '"'pass'"')" | python manage.py shell'
-docker-compose run --rm moonshine-api bash -c 'echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('"'admin'"', '"'admin@example.com'"', '"'pass'"')" | python manage.py shell'
+docker-compose run abtesting-service bash -c 'echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('"'admin'"', '"'admin@example.com'"', '"'pass'"')" | python manage.py shell'
+docker-compose run chrono bash -c 'echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('"'admin'"', '"'admin@example.com'"', '"'pass'"')" | python manage.py shell'
+docker-compose run moonshine-api bash -c 'echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('"'admin'"', '"'admin@example.com'"', '"'pass'"')" | python manage.py shell'
 
 heading 'Generate oauth app keys'
-docker-compose run --rm abtesting-service bash -c 'echo "from oauth2_provider.models import Application; Application.objects.create(client_id='"'abtesting-service-client-id'"', client_secret='"'abtesting-service-secret'"', user_id=1, client_type='"'public'"', name='"'admin app'"', authorization_grant_type='"'client-credentials'"')" | python manage.py shell'
-docker-compose run --rm chrono bash -c 'echo "from oauth2_provider.models import Application; Application.objects.create(client_id='"'chrono-client-id'"', client_secret='"'chrono-secret'"', user_id=1, client_type='"'public'"', name='"'admin app'"', authorization_grant_type='"'client-credentials'"')" | python manage.py shell'
-docker-compose run --rm moonshine-api bash -c 'echo "from oauth2_provider.models import Application; Application.objects.create(client_id='"'moonshine-api-client-id'"', client_secret='"'moonshine-api-secret'"', user_id=1, client_type='"'public'"', name='"'admin app'"', authorization_grant_type='"'client-credentials'"')" | python manage.py shell'
+docker-compose run abtesting-service bash -c 'echo "from oauth2_provider.models import Application; Application.objects.create(client_id='"'abtesting-service-client-id'"', client_secret='"'abtesting-service-secret'"', user_id=1, client_type='"'public'"', name='"'admin app'"', authorization_grant_type='"'client-credentials'"')" | python manage.py shell'
+docker-compose run chrono bash -c 'echo "from oauth2_provider.models import Application; Application.objects.create(client_id='"'chrono-client-id'"', client_secret='"'chrono-secret'"', user_id=1, client_type='"'public'"', name='"'admin app'"', authorization_grant_type='"'client-credentials'"')" | python manage.py shell'
+docker-compose run moonshine-api bash -c 'echo "from oauth2_provider.models import Application; Application.objects.create(client_id='"'moonshine-api-client-id'"', client_secret='"'moonshine-api-secret'"', user_id=1, client_type='"'public'"', name='"'admin app'"', authorization_grant_type='"'client-credentials'"')" | python manage.py shell'
 
 heading 'Import adventures for moonshine-api'
-docker-compose run --rm moonshine-api bash -c 'export PYTHONPATH=/usr/src/app && python feed/import_all.py env=dev'
+docker-compose run moonshine-api bash -c 'export PYTHONPATH=/usr/src/app && python feed/import_all.py env=dev'
 
 heading 'done!'
